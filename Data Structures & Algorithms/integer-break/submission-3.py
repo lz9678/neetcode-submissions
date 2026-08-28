ddc34cccba5a255475179e9a50eb96023e6a57c2
lz9678 <129570505+lz9678@users.noreply.github.com>
2026-08-28 00:12:51 -0400

    Add: integer-break - submission-3

---
+++ Data Structures & Algorithms/integer-break/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n == 2:
+            return 1
+        elif n == 3:
+            return 2
+
+        num = n // 3
+        remain = n % 3
+        if remain == 0:
+            return 3**num
+        elif remain == 1:
+            return 3**(num-1) * (3+remain)
+        else:
+            return 3**num * remain
